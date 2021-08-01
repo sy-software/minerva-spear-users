a8e930468a25721c348c648a4022179a2dc2ed32
Franklin Cruz <daisuke.sysoft@gmail.com>
2021-08-01 18:13:43 -0400

    Redefine auth services

--- internal/core/ports/services.go
+++ internal/core/ports/services.go
@@ -2,11 +2,12 @@ package ports
 
 import "github.com/sy-software/minerva-spear-users/internal/core/domain"
 
-type UsersService interface {
+// AuthService handle all action related to user life cycle
+type AuthService interface {
+	// Creates a minerva JWT for a user validated by an OAuth provider
 	Login(request domain.Login) (domain.UserToken, error)
-	Register(request domain.Login) (domain.UserToken, error)
-	Refresh(id string) (domain.User, error)
-
-	Get() (domain.User, error)
-	GetById(id string) (domain.User, error)
+	// Registers a user validated by an OAuth provider into minerva platform
+	Register(request domain.Register) (domain.UserToken, error)
+	// Refresh the current user token
+	Refresh(id string) (domain.UserToken, error)
 }
