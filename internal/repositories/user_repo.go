c0218f016b0ba47f381d064870f9127722579794
Franklin Cruz <daisuke.sysoft@gmail.com>
2021-08-03 17:39:14 -0400

    Remove commented code from example

--- internal/repositories/user_repo.go
+++ internal/repositories/user_repo.go
@@ -108,35 +108,3 @@ func (repo *UserRepo) GetByUsername(username string) (domain.User, error) {
 		Picture:  string(query.User.Picture),
 	}, nil
 }
-
-/*
-
-// create a client (safe to share across requests)
-client := graphql.NewClient("https://machinebox.io/graphql")
-
-// make a request
-req := graphql.NewRequest(`
-    query ($key: String!) {
-        items (id:$key) {
-            field1
-            field2
-            field3
-        }
-    }
-`)
-
-// set any variables
-req.Var("key", "value")
-
-// set header fields
-req.Header.Set("Cache-Control", "no-cache")
-
-// define a Context for the request
-ctx := context.Background()
-
-// run it and capture the response
-var respData ResponseStruct
-if err := client.Run(ctx, req, &respData); err != nil {
-    log.Fatal(err)
-}
-*/
